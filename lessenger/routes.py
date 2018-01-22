7421f4360c57a1de9c0964d0f9b60cd767d8002b
Allison Lyon <allison.lyon@alation.com>
2018-01-22 15:18:11 -0800

    add chat/messages POST endpoint and welcome response

--- lessenger/routes.py
+++ lessenger/routes.py
@@ -1,5 +1,22 @@
+from flask import request, jsonify
 from lessenger import app
 
 @app.route('/')
 def index():
     return "Hello, World!"
+
+@app.route('/chat/messages', methods = ['POST'])
+def lessenger():
+	if request.method == 'POST':
+		resp = {"messages": []}
+		data = request.form
+		if data['action'] == 'join': 
+			resp['messages'].append({
+				"type": "text", 
+				"text": "Welcome, {name}".format(name=request.form['name']),
+			})
+
+		elif data['action'] == 'message':
+			pass
+
+		return jsonify(resp)
